b19997a2665b5a2a806a0e5b01b214fa632d20c0
Marcos Vinícius Queiroz de Sant'Ana Filho <kieroth29@gmail.com>
2024-10-11 22:38:26 -0300

    chore: Subindo versão inicial do back-end mesclando boilerplate e padrão estabelecido previamente

---
+++ backend/src/schemas/user.ts
@@ -0,0 +1,16 @@
+export interface Student {
+    name: string
+    surname?: string
+    registration: string
+    email: string
+    advisor: string
+    photo: string
+    program: string
+}
+
+export interface Teacher {
+    name: string
+    surname?: string
+    registration: string
+    email: string
+}
