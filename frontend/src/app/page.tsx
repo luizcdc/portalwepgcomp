2425fa512459259090b61dea88ef536ea13decfc
GabrielCdev <gabi_borges01@hotmail.com>
2024-10-19 11:01:16 -0300

    feat: Import and use Header and Footer in mainPage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,13 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import LandingContainer from "@/pages/Landing/LandingContainer";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main>
+        <Header />
         <LandingContainer></LandingContainer>
+        <Footer />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        
-      </footer>
-    </div>
   );
 }
--- frontend/src/pages/Landing/LandingContainer.tsx
+++ frontend/src/pages/Landing/LandingContainer.tsx
@@ -3,7 +3,6 @@ import BotaoComponent from "@/components/UI/BotaoComponent";
 export default function LandingContainer() {
     return (
       <div>
-        <h1>{process.env.AMBIENTE}</h1>
         <BotaoComponent></BotaoComponent>
       </div>
     );
