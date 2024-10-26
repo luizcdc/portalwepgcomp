94c82fd3bc79610b56da012fa211bc6c06aff0bf
Marcos Vinícius Queiroz de Sant'Ana Filho <kieroth29@gmail.com>
2024-10-26 17:50:02 -0300

    chore: IC-26 Removing unused files from previous architecture

--- backend/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { loginController } from "../controllers";
-
-const router = express.Router();
-
-router.use("/login", loginController);
-export default router;
