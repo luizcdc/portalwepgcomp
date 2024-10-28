252e621ab0b8f3d91f177a67a578cf91bfb66101
jubsribs <julianacrispina@gmail.com>
2024-10-27 22:29:36 -0300

    feat: inserindo background

--- frontend/src/components/Orientacao.tsx
+++ frontend/src/components/Orientacao.tsx
@@ -1,39 +1,50 @@
+"use client";
+import backgroundOrientacao from "@/assets/images/background_orientacao.svg";
 
-export default function Orientacao(){
-    return(
-<div> 
-    <div className="orientacao-title">Orientações</div>
+export default function Orientacao() {
+  return (
+    <div className="d-flex flex-direction-column justify-content-center align-items-center "
+    style={{
+        backgroundImage: `url(${backgroundOrientacao.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        width: '1300px',
+        height: '500px'
+      }}>
+        
+      <div className="orientacaoTitle">Orientações</div>
 
-    <div className="orientacao-text">
-     <p className="orientacao-summary">
-      Este evento é uma excelente oportunidade para estudantes apresentarem e discutirem suas pesquisas, 
-      além de receberem feedback valioso de colegas e professores. É também uma chance de se envolver com a comunidade acadêmica 
-      e contribuir para o avanço do conhecimento na área de Ciência da Computação.
-     </p>
+      <div className="orientacaoText">
+        <p className="orientacaoSummary">
+          Este evento é uma excelente oportunidade para estudantes apresentarem
+          e discutirem suas pesquisas, além de receberem feedback valioso de
+          colegas e professores. É também uma chance de se envolver com a
+          comunidade acadêmica e contribuir para o avanço do conhecimento na
+          área de Ciência da Computação.
+        </p>
 
-     <p className="orientacao-important">
-      Datas Importantes:
-     </p>
+        <p className="orientacaoImportant">Datas Importantes:</p>
 
-     <div className="orientacao-date">
-        <i className="bi bi-circle-fill"></i>
-      <p className="orientacao-text-date">
-      Data limite para submissão: 27 de outubro de 2024.
-      </p>
-     </div>
+        <div className="orientacaoDate">
+          <i className='bi bi-circle-fill'></i>
+          <p className="orientacaoTextDate">
+            Data limite para submissão: 27 de outubro de 2024.
+          </p>
+        </div>
 
-     <div className="orientacao-date">
-        <i className="bi bi-circle-fill"></i>
-      <p className="orientacao-text-date">
-       O evento será realizado de 12 a 14 de novembro de 2024.
-      </p>
-     </div>
-    
+        <div className="orientacaoDate">
+          <i className='bi bi-circle-fill'></i>
+          <p className="orientacaoTextDate">
+            O evento será realizado de 12 a 14 de novembro de 2024.
+          </p>
+        </div>
+      </div>
+
+      <div className="orientacaoButton">
+        <button className="orientacaoButtonText">
+          Ver todas as orientações
+        </button>
+      </div>
     </div>
-
-    <div className="orientacao-button">
-     <button className="orientacao-button-text">Ver todas as orientações</button>
-    </div>
-</div>
-    );
+  );
 }
