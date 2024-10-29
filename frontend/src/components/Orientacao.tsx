a94a0bcdfd547d3df1cfff5d7912388ffe93524a
jubsribs <julianacrispina@gmail.com>
2024-10-28 22:40:57 -0300

    feat: estilização do css
    Co-authored-by: HenriqueHatakeyama <HenriqueTorresHatakeyama@users.noreply.github.com>

--- frontend/src/components/Orientacao.tsx
+++ frontend/src/components/Orientacao.tsx
@@ -3,47 +3,95 @@ import backgroundOrientacao from "@/assets/images/background_orientacao.svg";
 
 export default function Orientacao() {
   return (
-    <div className="d-flex flex-direction-column justify-content-center align-items-center "
-    style={{
+    <div
+      style={{
         backgroundImage: `url(${backgroundOrientacao.src})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-        width: '1300px',
-        height: '500px'
-      }}>
-        
-      <div className="orientacaoTitle">Orientações</div>
+        width: "1300px",
+        height: "500px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white"
+      }}
+    >
+      <div className='text-white text-align-center fs-2 fw-bold'>
+        Orientações
+      </div>
 
-      <div className="orientacaoText">
-        <p className="orientacaoSummary">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "25px",
+        }}
+      >
+        <div>
           Este evento é uma excelente oportunidade para estudantes apresentarem
-          e discutirem suas pesquisas, além de receberem feedback valioso de
-          colegas e professores. É também uma chance de se envolver com a
-          comunidade acadêmica e contribuir para o avanço do conhecimento na
+          e discutirem suas pesquisas, além de {"\n"} receberem feedback valioso
+          de colegas e professores.{"\n"} É também uma chance de se envolver com
+          a comunidade acadêmica e contribuir para o avanço do conhecimento na
           área de Ciência da Computação.
-        </p>
-
-        <p className="orientacaoImportant">Datas Importantes:</p>
-
-        <div className="orientacaoDate">
-          <i className='bi bi-circle-fill'></i>
-          <p className="orientacaoTextDate">
-            Data limite para submissão: 27 de outubro de 2024.
-          </p>
         </div>
+        
+        <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+        >
+          <div className='fw-bold'>Datas Importantes:</div>
 
-        <div className="orientacaoDate">
-          <i className='bi bi-circle-fill'></i>
-          <p className="orientacaoTextDate">
-            O evento será realizado de 12 a 14 de novembro de 2024.
-          </p>
+          <div className='d-flex flex-diretcion-row align-items-center gap-2'>
+            <div
+              style={{
+                width: "6px",
+                height: "6px",
+                borderRadius: "10px",
+                backgroundColor: "white",
+              }}
+            ></div>
+            <div className='orientacaoTextDate'>
+              Data limite para submissão: 27 de outubro de 2024.
+            </div>
+          </div>
+
+          <div className='d-flex flex-direction-row align-items-center gap-2'>
+            <div
+              style={{
+                width: "6px",
+                height: "6px",
+                borderRadius: "10px",
+                backgroundColor: "white",
+              }}
+            ></div>
+            <div className='orientacaoTextDate'>
+              O evento será realizado de 12 a 14 de novembro de 2024.
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="orientacaoButton">
-        <button className="orientacaoButtonText">
-          Ver todas as orientações
-        </button>
+      <div
+      style={{
+        border: "solid",
+        borderColor: "white",
+        width: "300px",
+        height: "60px",
+        borderRadius: "12px",
+        borderWidth: "1px",
+        cursor: "pointer",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontWeight: "bold",
+        marginTop: "40px"
+      }}>
+        Ver todas as orientações
       </div>
     </div>
   );
