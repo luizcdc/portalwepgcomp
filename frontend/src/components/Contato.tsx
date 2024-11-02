4d6c0bc280a68019499d51772605bfeac4c2bf63
jubsribs <julianacrispina@gmail.com>
2024-11-01 22:10:27 -0300

    feat: ajustes de estilazação  e juntanda dos componentes de local do evento.
    Co-authored-by: HenriqueHatakeyama <HenriqueTorresHatakeyama@users.noreply.github.com>

--- frontend/src/components/Contato.tsx
+++ frontend/src/components/Contato.tsx
@@ -1,82 +1,109 @@
-"use client"
-import {Poppins} from "next/font/google"
+"use client";
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   weight: ["400", "600"],
   subsets: ["latin"],
 });
 
-export default function Contato(){
-    return(
-        <div style={{
-        fontFamily:poppins.style.fontFamily,
+export default function Contato() {
+  return (
+    <div
+      style={{
+        fontFamily: poppins.style.fontFamily,
         display: "flex",
         flexDirection: "column",
         alignItems: "left",
-        gap: "20px"
-      }}>
-            <div className="fs-1 fw-bold text-left">Contato</div>
-            
-            <div
+        gap: "20px",
+      }}
+    >
+      <div className='fs-1 fw-bold text-left text-white'>Contato</div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "20px",
+          alignItems: "flex-start",
+        }}
+      >
+        <div>
+          <label className='form-label fs-6 fw-semibold text-white'>
+            Nome:
+          </label>
+          <input
+            type='nome'
+            className='form-control'
+            id='name'
+            placeholder='Insira seu nome'
             style={{
-                display: "flex",
-                flexDirection: "row",
-                gap: "20px",
-                alignItems: "flex-start"
+              width: "300px",
+              height: "40px",
+              borderWidth: "1px",
+              borderColor: "white",
+              backgroundColor: "#0074BA",
             }}
-            >
-                
-                <div>
-                    <label className="form-label fs-6 fw-semibold">Nome:</label>
-                    <input type="nome" className="form-control" id="name" placeholder="Insira seu nome"
-                    style={{
-                        width: "300px",
-                        height: "40px",
-                        borderWidth: "2px",
-                    }} />
-                </div>
-                
-                <div>
-                    <label className="form-label fs-6 fw-semibold">E-mail:</label>
-                    <input type="email" className="form-control" id="email" placeholder="Insira seu e-mail"
-                    style={{
-                        width: "300px",
-                        height: "40px",
-                        borderWidth: "2px",
-                    }} />
-                </div>
-
-            </div>
-
-            <div
->
-                <label className="form-label fs-6 fw-semibold">Mensagem:</label>
-                <input type="text" className="form-control" id="message" placeholder="Digite sua mensagem"
-                style={{
-                    width: "620px",
-                    height: "137px",
-                    borderWidth: "2px",
-                }} />
-            </div>
-
-            <div
-            style={{
-                border: "solid",
-                backgroundColor: "blue",
-                color: "white",
-                borderColor: "blue",
-                borderRadius: "8px",
-                width: "620px",
-                height: "38px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginBottom: "20px",
-                fontSize: "16px",
-                fontWeight: "semibold",
-            }}
-            >Entrar</div>
-
+          />
         </div>
-    )
+
+        <div>
+          <label className='form-label fs-6 fw-semibold text-white'>
+            E-mail:
+          </label>
+          <input
+            type='email'
+            className='form-control'
+            id='email'
+            placeholder='Insira seu e-mail'
+            style={{
+              width: "300px",
+              height: "40px",
+              borderWidth: "1px",
+              borderColor: "white",
+              backgroundColor: "#0074BA",
+            }}
+          />
+        </div>
+      </div>
+
+      <div>
+        <label className='form-label fs-6 fw-semibold text-white'>
+          Mensagem:
+        </label>
+        <input
+          type='text'
+          className='form-control'
+          id='message'
+          placeholder='Digite sua mensagem'
+          style={{
+            width: "620px",
+            height: "137px",
+            borderWidth: "1px",
+            borderColor: "white",
+            backgroundColor: "#0074BA",
+          }}
+        />
+      </div>
+
+      <div
+        style={{
+          border: "solid",
+          backgroundColor: "white",
+          color: "#054B75",
+          borderColor: "white",
+          borderRadius: "8px",
+          width: "620px",
+          height: "38px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: "20px",
+          fontSize: "16px",
+          fontWeight: "bold",
+        }}
+      >
+        Entrar
+      </div>
+    </div>
+  );
 }
