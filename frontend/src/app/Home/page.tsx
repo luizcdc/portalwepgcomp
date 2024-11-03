70031f3a172668b97147ac207488fd51e8988156
jubsribs <julianacrispina@gmail.com>
2024-11-02 23:00:50 -0300

    fix: reunindo todos os componentes na tela home.

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -3,15 +3,22 @@
 import Carousel from "@/components/Carousel/Carousel";
 import LocalEvento from "@/components/LocalEvento";
 import Orientacao from "@/components/Orientacao";
+import Realizacao from "@/components/Realizacao";
 import SearchGroups from "@/components/SearchGroups";
 
 export default function Home() {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <Carousel />
       <Orientacao />
       <SearchGroups />
       <LocalEvento />
+      <Realizacao />
     </div>
   );
 }
