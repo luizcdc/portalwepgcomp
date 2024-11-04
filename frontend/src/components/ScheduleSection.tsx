c21821a92e374b00cc60107876d79e21fe24cfc0
Rezende123 <feliperezende.srz@gmail.com>
2024-11-04 16:50:26 -0300

    Corrige texto

--- frontend/src/components/ScheduleSection.tsx
+++ frontend/src/components/ScheduleSection.tsx
@@ -61,7 +61,7 @@ export default function ScheduleSection() {
             marginBottom: "40px",
           }}
         >
-          Programação (EDITADO 1)
+          Programação
         </h1>
         <div style={{ display: "flex", gap: "30px", justifyContent: "center" }}>
           <button
