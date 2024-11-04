9bd6c82f2f7b7229f15385361bd24476df09cfae
Rezende123 <feliperezende.srz@gmail.com>
2024-11-04 16:32:05 -0300

    Altera texto para teste

--- frontend/src/components/ScheduleSection.tsx
+++ frontend/src/components/ScheduleSection.tsx
@@ -61,7 +61,7 @@ export default function ScheduleSection() {
             marginBottom: "40px",
           }}
         >
-          Programação
+          Programa
         </h1>
         <div style={{ display: "flex", gap: "30px", justifyContent: "center" }}>
           <button
