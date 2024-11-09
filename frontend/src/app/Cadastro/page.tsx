da2ae06a4a8da3c6809623508a3de5f8b9002924
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-08 21:55:54 -0300

    feat: Added style file to Cadastro

--- frontend/src/app/Cadastro/page.tsx
+++ frontend/src/app/Cadastro/page.tsx
@@ -1,16 +1,17 @@
 "use client"
 
 import { FormCadastro } from "@/components/Forms/FormCadastro";
+import "./style.scss";
 
 export default function Cadastro() {
     return (
-        <div style={{ color: "black" }}>
-            <div className="container" >
-                <h1 className="d-flex justify-content-center mt-5 fw-normal" style={{ fontSize: "3rem", color: "#0066BA" }}>WEPGCOMP
-                    <span className="ms-2" style={{ fontSize: "3rem", color: "#0066BA" }}>2025</span>
+        <div className="text-black">
+            <div className="container">
+                <h1 className="d-flex justify-content-center mt-5 fw-normal">WEPGCOMP
+                    <span className="ms-2">2025</span>
                 </h1>
-                <hr style={{ border: "2px solid #FFA90F" }}></hr>
-                <h2 className="d-flex justify-content-center mb-4 fw-bold text-black" style={{ fontSize: "3rem" }}>Cadastro</h2>
+                <hr />
+                <h2 className="d-flex justify-content-center mb-4 fw-bold text-black">Cadastro</h2>
             </div>
             <div className="container d-flex justify-content-center mb-5">
                 <FormCadastro />
