5898b63e4cd3a56873a745c59c4bb6654a3924a7
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-09 17:42:49 -0300

    style: Improvend style into Login page

--- frontend/src/app/Login/page.tsx
+++ frontend/src/app/Login/page.tsx
@@ -7,12 +7,12 @@ export default function Login() {
   return (
     <div className="container d-flex flex-column flex-grow-1 text-black">
       <div className="container">
-        <h1 className="d-flex justify-content-center mt-5 fw-normal text-primary">
+        <h1 className="d-flex justify-content-center mt-5 fw-normal">
           WEPGCOMP
-          <span className="fw-bold text-primary ms-2">2025</span>
+          <span className="ms-2">2025</span>
         </h1>
-        <hr className="border border-warning border-2"></hr>
-        <h2 className="d-flex justify-content-center mb-4">Acesse sua conta</h2>
+        <hr />
+        <h2 className="d-flex justify-content-center mb-4 fw-semibold">Acesse sua conta</h2>
       </div>
 
       <div className="container d-flex justify-content-center mb-5">
