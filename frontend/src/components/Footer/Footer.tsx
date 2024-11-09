9aa177d893de77515936b4adcc7323a945de02c4
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-09 16:26:19 -0300

    style: Added style file to Footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import "./style.scss";
+
+export default function Footer() {
+    return (
+        <footer className="text-center text-white pt-1">
+            <div className="footer-text">© 2025 WEPGCOMP</div>
+        </footer>
+    );
+};
