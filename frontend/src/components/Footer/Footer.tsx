5df9f66ea9997233933de7abc11d29679cc3abef
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-09 17:38:05 -0300

    fix: Fixing classNames into Cadastro and Footer files

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import "./style.scss";
 
 export default function Footer() {
     return (
-        <footer className="footer navbar-fixed-bottom text-center text-white pt-1">
+        <footer className="navbar-fixed-bottom text-center text-white pt-1">
             <div className="footer-text">© 2025 WEPGCOMP</div>
         </footer>
     );
