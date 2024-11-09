c611a8d3ed9fb9f319af4a3194c8774650eabdb2
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-08 21:26:22 -0300

    style: Updated SCSS into 'Cadastro'

--- frontend/src/app/Cadastro/page.tsx
+++ frontend/src/app/Cadastro/page.tsx
@@ -4,13 +4,13 @@ import { FormCadastro } from "@/components/Forms/FormCadastro";
 
 export default function Cadastro() {
     return (
-        <div style={{color: "black"}}>
+        <div style={{ color: "black" }}>
             <div className="container" >
-                <h1 className="d-flex justify-content-center mt-5 fw-normal text-primary">WEPGCOMP
-                    <span className="fw-bold text-primary ms-2">2025</span>
+                <h1 className="d-flex justify-content-center mt-5 fw-normal" style={{ fontSize: "3rem", color: "#0066BA" }}>WEPGCOMP
+                    <span className="ms-2" style={{ fontSize: "3rem", color: "#0066BA" }}>2025</span>
                 </h1>
-                <hr className="border border-warning border-2"></hr>
-                <h2 className="d-flex justify-content-center mb-4">Cadastro</h2>
+                <hr style={{ border: "2px solid #FFA90F" }}></hr>
+                <h2 className="d-flex justify-content-center mb-4 fw-bold text-black" style={{ fontSize: "3rem" }}>Cadastro</h2>
             </div>
             <div className="container d-flex justify-content-center mb-5">
                 <FormCadastro />
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
         <div className="footer navbar-fixed-bottom text-center text-white pt-2"
-        style={{
-            backgroundColor: "#AAAAAA",
-        }}>
-            <div>© 2025 WEPGCOMP</div>
+            style={{
+                backgroundColor: "#AAAAAA",
+            }}>
+            <div style={{ fontSize: "1.5rem" }}>© 2025 WEPGCOMP</div>
         </div>
     );
 };
