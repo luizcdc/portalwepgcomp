565879d00069ce4bb25ccc47f71d154c6328ab24
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-09 02:08:35 -0300

    feat: Added validation into 'Contato' form

---
+++ frontend/src/components/Contato/Contato.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { FormContato } from "../Forms/Contato/FormContato";
+
+export default function Contato() {
+  return (
+    <div className="d-flex flex-column align-items-start">
+      <div className="container">
+        <div className="fs-1 fw-bold text-white mb-3">Contato</div>
+        <FormContato />
+      </div>
+    </div>
+  );
+}
