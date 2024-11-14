afd661888fa0590bebabcf886609614c66cfcb32
Silas Cardoso <silasnunes105@gmail.com>
2024-11-14 14:16:39 -0300

    IC-87 - Implementação dos labels

--- frontend/src/components/ModalEdicaoApresentacao/ModalEdicaoApresentacao.tsx
+++ frontend/src/components/ModalEdicaoApresentacao/ModalEdicaoApresentacao.tsx
@@ -18,7 +18,48 @@ export default function ModalEdicaoApresentacao(){
             onConfirm={handleSendEdicao}
         >
             <div>
-            Hola
+                <h1 className="d-flex justify-content-center mt-5 fw-normal title-alterar-senha">
+                    WEPGCOMP
+                    <span className="ms-2">2025</span>
+                </h1>
+                <hr />
+
+                <div>
+                    <div>
+                        <label>Tema</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label>Abstract</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label>Área de atuação</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label>Horário sugerido para a apresentação</label>
+                        <input type="calendar" />
+                    </div>
+                    <div>
+                        <label>Slide da apresentação</label>
+                        <input type="upload" />
+                    </div>
+                    <div>
+                        <label>Orientador</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label>Insira uma foto sua</label>
+                        <input type="upload" />
+                    </div>
+
+                    <div>
+                        <button type="button">Editar</button>
+                    </div>
+                    
+                </div>
+
             </div>
 
 
