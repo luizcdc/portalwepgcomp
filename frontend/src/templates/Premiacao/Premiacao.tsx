b9d71ccdde872ff258653ecd7e976dbb29da5184
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-15 14:53:22 -0300

    style: Added empty list validation

--- frontend/src/templates/Premiacao/Premiacao.tsx
+++ frontend/src/templates/Premiacao/Premiacao.tsx
@@ -32,15 +32,29 @@ export default function Premiacao({ titulo, descricao, premiacoes, medalWidth, m
         <h1 className="fw-bold title">{titulo}</h1>
         <h5 className="text-black">{descricao}</h5>
 
-        {premiacoes.map((item, index) => (
-          <div key={index} className="d-flex align-items-center justify-content-between border border-3 border-solid custom-border p-3 mt-4 me-5">
-            <div className="text-black">
-              <p className={`text-black fw-semibold ${!item.subtitulo ? 'mb-0' : ''}`}>{item.titulo}</p>
-              {item.subtitulo && <p className="text-black mb-0">{item.subtitulo}</p>}
-            </div>
-            <MedalhaIcon tipo={item.medalha} width={medalWidth} height={medalHeight} />
+        {premiacoes.length === 0 ? (
+          <div className="d-flex align-items-center justify-content-center p-3 mt-4 me-5">
+            <p className="empty-list mb-0">
+              <Image
+                src="/assets/images/empty_box.svg"
+                alt="Lista vazia"
+                width={90}
+                height={90}
+              />
+              Essa lista ainda está vazia
+            </p>
           </div>
-        ))}
+        ) : (
+          premiacoes.map((item, index) => (
+            <div key={index} className="d-flex align-items-center justify-content-between border border-3 border-solid custom-border p-3 mt-4 me-5">
+              <div className="text-black">
+                <p className={`text-black fw-semibold ${!item.subtitulo ? 'mb-0' : ''}`}>{item.titulo}</p>
+                {item.subtitulo && <p className="text-black mb-0">{item.subtitulo}</p>}
+              </div>
+              <MedalhaIcon tipo={item.medalha} width={medalWidth} height={medalHeight} />
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
