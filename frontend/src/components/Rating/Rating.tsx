9b1529ea200a94e9ced74a46097f1e06f1200ac8
Henrique Torres <henriquetorres0910@gmail.com>
2024-11-17 00:37:44 -0300

    Correção de alguns erros

--- frontend/src/components/Rating/Rating.tsx
+++ frontend/src/components/Rating/Rating.tsx
@@ -12,23 +12,23 @@ export default function Rating(){
 
         <div className="rating">
             <div className="star" onClick={() => setRating(1)}>
-                <StarRating color={rating>=1?"#F17F0C":"#555555"} />
+                <StarRating color={rating>=1?"#FFA90F":"#555555"} />
             </div>
 
             <div className="star" onClick={() => setRating(2)}>
-                <StarRating color={rating>=2?"#F17F0C":"#555555"}/>
+                <StarRating color={rating>=2?"#FFA90F":"#555555"}/>
             </div>
 
             <div className="star" onClick={() => setRating(3)}>
-                <StarRating color={rating>=3?"#F17F0C":"#555555"}/>
+                <StarRating color={rating>=3?"#FFA90F":"#555555"}/>
             </div>
 
             <div className="star" onClick={() => setRating(4)}>
-                <StarRating color={rating>=4?"#F17F0C":"#555555"}/>
+                <StarRating color={rating>=4?"#FFA90F":"#555555"}/>
             </div>
 
             <div className="star" onClick={() => setRating(5)}>
-                <StarRating color={rating>=5?"#F17F0C":"#555555"}/>
+                <StarRating color={rating>=5?"#FFA90F":"#555555"}/>
             </div>
         </div>
     );
