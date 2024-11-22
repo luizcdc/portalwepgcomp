0fff2ea97d72838da721ad00ba3e560f06a9db8f
Henrique Torres <henriquetorres0910@gmail.com>
2024-11-22 13:28:35 -0300

    Personalização da ProtectedLayout (e uma adição no package-lock.json)

--- frontend/src/components/ProtectedLayout/protectedLayout.tsx
+++ frontend/src/components/ProtectedLayout/protectedLayout.tsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { AuthContext } from "@/context/AuthProvider/authProvider";
+import "./protectedLayout.css";
 
 export const ProtectedLayout = ({ children }: { children:  React.ReactNode}) => {
     const {signed} = useContext(AuthContext);
 
     
     if(!signed){
-        return <h1>Você não possui acesso!</h1>;
+        return <h1>Você não possui acesso!<p>Para acessar esta página, entre com sua conta em Login</p></h1>;
     }
 
     return children;
