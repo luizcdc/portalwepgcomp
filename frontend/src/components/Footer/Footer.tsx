1c9c25af58deff1f0cbca11214a3760bd5981ef7
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-23 14:59:58 -0300

    style: Modified scss class fonts to inline fontsize into Footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import "./style.scss";
 
 export default function Footer() {
     return (
-        <footer className="navbar-fixed-bottom text-center text-white pt-1">
-            <div className="footer-text">© 2025 WEPGCOMP</div>
+        <footer className="navbar-fixed-bottom text-center text-white pt-2">
+            <h4>© 2025 WEPGCOMP</h4>
         </footer>
     );
 };
