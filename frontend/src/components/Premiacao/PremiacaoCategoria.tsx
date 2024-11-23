44a3eae8902a6aba0720c28676b1fc53154f3cdc
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-23 14:44:13 -0300

    feat: Added 'nota' property to 'Premiacoes' mock and 'PremiacaoCategoria'

--- frontend/src/components/Premiacao/PremiacaoCategoria.tsx
+++ frontend/src/components/Premiacao/PremiacaoCategoria.tsx
@@ -3,10 +3,10 @@ import Premiacao from "@/templates/Premiacao/Premiacao";
 type PremiacaoCategoriaProps = {
     titulo: string;
     descricao: string;
-    premiacoes: { titulo: string; subtitulo: string}[];
+    premiacoes: { titulo: string; subtitulo: string; nota: number }[];
 };
 
-export default function PremiacaoCategoria({ titulo, descricao, premiacoes}: PremiacaoCategoriaProps) {
+export default function PremiacaoCategoria({ titulo, descricao, premiacoes }: PremiacaoCategoriaProps) {
     return (
         <Premiacao
             titulo={titulo}
