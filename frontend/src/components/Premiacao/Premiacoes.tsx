41806008d9bc32c53c0ed0a32b11f675a911c6b7
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-23 12:22:51 -0300

    fix: Modified Premiacao page design

--- frontend/src/components/Premiacao/Premiacoes.tsx
+++ frontend/src/components/Premiacao/Premiacoes.tsx
@@ -1,48 +1,49 @@
 import PremiacaoCategoria from './PremiacaoCategoria';
 
-export default function Premiacoes() {
-    const premiacoesPublico: { titulo: string; subtitulo: string; medalha: 'gold' | 'silver' | 'bronze' }[] = [
-        // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)', medalha: 'gold' },
-        // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)', medalha: 'silver' },
-        // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)', medalha: 'bronze' },
+export default function Premiacoes({ categoria }: { categoria: "banca" | "avaliadores" | "publico" }) {
+    const premiacoesPublico: { titulo: string; subtitulo: string; }[] = [
+        // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+        // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+        // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     ];
 
-    const premiacoesBanca: { titulo: string; subtitulo: string; medalha: 'gold' | 'silver' | 'bronze' }[] = [
-        { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)', medalha: 'gold' },
-        { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)', medalha: 'silver' },
-        { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)', medalha: 'bronze' },
+    const premiacoesBanca: { titulo: string; subtitulo: string; }[] = [
+        { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+        { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+        { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     ];
-    
-    const premiacoesAvaliadores: { titulo: string; medalha: 'gold' | 'silver' | 'bronze' }[] = [
-        { titulo: 'Nome do usuário', medalha: 'gold' },
-        { titulo: 'Nome do usuário', medalha: 'silver' },
-        { titulo: 'Nome do usuário', medalha: 'bronze' },
+
+    const premiacoesAvaliadores: { titulo: string; subtitulo: string; }[] = [
+        { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+        { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+        { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     ];
+
+    const getAwards = () => {
+        switch (categoria) {
+            case "publico":
+                return premiacoesPublico;
+            case "banca":
+                return premiacoesBanca;
+            case "avaliadores":
+                return premiacoesAvaliadores;
+            default:
+                return [];
+        }
+    };
+
 
     return (
         <div className="d-flex flex-column premiacao-list">
             <PremiacaoCategoria
-                titulo="Melhores Apresentações - Público"
-                descricao="Ranking das melhores apresentações por voto da audiência"
-                premiacoes={premiacoesPublico}
-                medalWidth={50}
-                medalHeight={50}
-            />
-
-            <PremiacaoCategoria
-                titulo="Melhores Apresentações - Banca"
-                descricao="Ranking das melhores apresentações por voto da banca avaliadora"
-                premiacoes={premiacoesBanca}
-                medalWidth={50}
-                medalHeight={50}
-            />
-            
-            <PremiacaoCategoria
-                titulo="Melhores Avaliadores"
-                descricao="Ranking dos melhores/maiores avaliadores da edição"
-                premiacoes={premiacoesAvaliadores}
-                medalWidth={35}
-                medalHeight={35}
+                titulo={`Melhores Apresentações - ${categoria === 'banca' ? 'Banca' : categoria === 'avaliadores' ? 'Avaliadores' : 'Público'}`}
+                descricao={`
+                    ${categoria === 'banca' ? 'Ranking das melhores apresentações por voto da banca avaliadora'
+                        : categoria === 'avaliadores' ? 'Ranking dos melhores/maiores avaliadores da edição'
+                            : categoria === 'publico' ? 'Ranking das melhores apresentações por voto da audiência'
+                                : ''}
+                    `}
+                premiacoes={getAwards()}
             />
         </div>
     );
