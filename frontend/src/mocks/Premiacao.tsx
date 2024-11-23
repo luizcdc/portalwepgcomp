c80e334ca83a95a8d96b299e9f14d845ea94286a
GabrielCdev <gabi_borges01@hotmail.com>
2024-11-23 14:24:24 -0300

    style: Improved mock list

--- frontend/src/mocks/Premiacao.tsx
+++ frontend/src/mocks/Premiacao.tsx
@@ -2,15 +2,21 @@ export const premiacoesBancaMock: { titulo: string; subtitulo: string; }[] = [
     { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+    { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+    { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
 ];
 
 export const premiacoesAvaliadoresMock: { titulo: string; subtitulo: string; }[] = [
     { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+    { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+    { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
 ];
 
 export const premiacoesPublicoMock: { titulo: string; subtitulo: string; }[] = [
+    // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
+    // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
     // { titulo: 'Nome da apresentação', subtitulo: 'Nome do(a) autor(a)' },
