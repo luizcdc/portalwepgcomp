2e1914cf0514b88b0ab8307457eb7f09f119b882
paloma.passos <palomabcp06@gmail.com>
2024-11-30 16:35:25 -0300

    fix: build

--- frontend/src/app/Favoritos/page.tsx
+++ frontend/src/app/Favoritos/page.tsx
@@ -3,12 +3,10 @@
 import { ProtectedLayout } from "@/components/ProtectedLayout/protectedLayout";
 import { FavoritosMock } from "@/mocks/Favoritos";
 import Listagem from "@/templates/Listagem/Listagem";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Favoritos() {
   const { title, userArea, cardsMock } = FavoritosMock;
-  const router = useRouter();
 
   const [searchValue, setSearchValue] = useState<string>("");
   const [sessionsListValues, setSessionsListValues] =
