b6f5d30ad9b8db6fa46e9924d74159b70deefdaa
Rezende123 <feliperezende.srz@gmail.com>
2024-12-01 10:46:24 -0300

    Define variáveis de ambiente a partir do secrets

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,65 +7,63 @@ import { Transport } from '@nestjs/microservices';
 import { queueConstants } from './queue/constants';
 import metadata from './metadata';
 
-export default function bootstrap() {
-  NestFactory.create(AppModule).then((app) => {
-    app.enableCors({
-      origin: [
-        'https://portal-wepgcomp-client-development.vercel.app',
-        'https://portal-wepgcomp-client.vercel.app',
-        `http://localhost:${process.env.FRONTEND_LOCAL_PORT}`
-      ],
-      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
-    });
-  
-    const config = new DocumentBuilder()
-      .setTitle('Documentação de Rotas do PortalWEPGCOMP')
-      .setDescription(
-        'Especificação e descrição das rotas da API do projeto PortalWEPGCOMP.',
-      )
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
-    SwaggerModule.loadPluginMetadata(metadata).then(() => {
-      const documentFactory = () => SwaggerModule.createDocument(app, config);
-      SwaggerModule.setup('docs', app, documentFactory, {
-        customSiteTitle: 'Api Docs',
-        customJs: [
-          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-        ],
-        customCssUrl: [
-          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-        ],
-      });
-    });
-  
-    app.useGlobalPipes(
-      new ValidationPipe({
-        transform: true,
-      }),
-    );
-    app.useGlobalFilters(new HttpExceptionFilter());
-  
-    for (const queue of queueConstants.queues) {
-      app.connectMicroservice({
-        transport: Transport.RMQ,
-        options: {
-          urls: [process.env.QUEUE_URL],
-          queue: queue,
-          noAck: false,
-          queueOptions: {
-            durable: true,
-          },
-        },
-      });
-    }
-  
-    app.startAllMicroservices().then(() => {
-      app.listen(process.env.PORT ?? 3000).then(() => {});
-    });
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'https://portal-wepgcomp-client-development.vercel.app',
+      'https://portal-wepgcomp-client.vercel.app',
+      `http://localhost:${process.env.FRONTEND_LOCAL_PORT}`
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação de Rotas do PortalWEPGCOMP')
+    .setDescription(
+      'Especificação e descrição das rotas da API do projeto PortalWEPGCOMP.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  await SwaggerModule.loadPluginMetadata(metadata);
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    customSiteTitle: 'Api Docs',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  for (const queue of queueConstants.queues) {
+    app.connectMicroservice({
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.QUEUE_URL],
+        queue: queue,
+        noAck: false,
+        queueOptions: {
+          durable: true,
+        },
+      },
+    });
+  }
+
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
