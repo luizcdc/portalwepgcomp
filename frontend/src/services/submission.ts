49036c2903cce211f25b69b900091fb3e671a62a
GabrielCdev <gabi_borges01@hotmail.com>
2024-12-09 12:33:46 -0300

    feat: Created Routes and Services to Submission

---
+++ frontend/src/services/submission.ts
@@ -0,0 +1,47 @@
+import { GetSubmissionParams, SubmissionParams } from '@/models/submission';
+import { axiosInstance } from '@/utils/api';
+import { UUID } from 'crypto';
+
+const baseUrl = "/submision";
+
+export const submissionApi = {
+    getSubmissions: async (params: GetSubmissionParams) => {
+        const instance = axiosInstance();
+
+        const { data } = await instance.get(`${baseUrl}`, { params });
+
+        return data;
+    },
+
+    getSubmissionById: async (idSubmission: UUID) => {
+        const instance = axiosInstance();
+
+        const { data } = await instance.get(`${baseUrl}/${idSubmission}`);
+
+        return data;
+    },
+
+    createSubmission: async (body: SubmissionParams) => {
+        const instance = axiosInstance();
+
+        const { data } = await instance.post(`${baseUrl}`, body);
+
+        return data;
+    },
+
+    updateSubmissionById: async (idSubmission: UUID, body: SubmissionParams) => {
+        const instance = axiosInstance();
+
+        const { data } = await instance.put(`${baseUrl}/${idSubmission}`, body);
+
+        return data;
+    },
+
+    deleteSubmissionById: async (idSubmission: UUID) => {
+        const instance = axiosInstance();
+
+        const { data } = await instance.delete(`${baseUrl}/${idSubmission}`);
+
+        return data;
+    },
+}
