ec63e0f2c33c4aada3efe1d125a22106e80832b3
Lucas de Azevedo <77306471+LucasAzoli@users.noreply.github.com>
2024-12-10 18:02:10 -0300

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -18,6 +18,9 @@ const hostname = typeof window !== "undefined" ? window.location.hostname : "";
 const domain = hostname?.split(".")[0];
 
 const apis = {
+    localhost: "https://portal-wepgcomp-api-development.vercel.app/",
+  "portal-wepgcomp-client-development":
+    "https://portal-wepgcomp-api-development.vercel.app/",
   "portal-wepgcomp-client": "https://portal-wepgcomp-api.vercel.app/",
 };
 
