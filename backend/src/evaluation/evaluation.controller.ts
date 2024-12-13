96c911ab07d37d83f6bfa667ba2d3148b83bdad7
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-12-12 21:41:21 -0300

    doc: Improve display and comments in swagger for Evaluation controller

--- backend/src/evaluation/evaluation.controller.ts
+++ backend/src/evaluation/evaluation.controller.ts
@@ -1,16 +1,36 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { EvaluationService } from './evaluation.service';
 import { CreateEvaluationDto } from './dto/create-evaluation.dto';
+import { ApiBody, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 @Controller('evaluations')
 export class EvaluationController {
   constructor(private readonly evaluationService: EvaluationService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Create new evaluations.',
+  })
+  @ApiBody({
+    type: [CreateEvaluationDto],
+    description:
+      'Array of evaluations (one for each criteria), all for the same submissionId.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The evaluations have been created successfully.',
+  })
   async create(@Body() evaluations: CreateEvaluationDto[]) {
     return await this.evaluationService.create(evaluations);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Find evaluations' })
+  @ApiQuery({
+    name: 'userId',
+    required: false,
+    description: 'Filter evaluations by user ID',
+  })
+  @ApiResponse({ status: 200, description: 'Return evaluations.' })
   async find(@Query('userId') userId?: string) {
     if (userId) {
       return await this.evaluationService.findOne(userId);
