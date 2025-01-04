3662110cf5cf72a2cfc3d127240d9142c645bf5c
ernestosrf <ernestosrf98@gmail.com>
2025-01-04 13:39:20 -0300

    feat: ICI-181 update Guidance model to use cascading delete and enhance Edicoes component to fetch and display event editions dinamic

--- frontend/src/app/edicoes/page.tsx
+++ frontend/src/app/edicoes/page.tsx
@@ -1,13 +1,30 @@
 "use client";
 import Listagem from "@/templates/Listagem/Listagem";
-import { EventoMock } from "@/mocks/Evento";
 import { useRouter } from "next/navigation";
 import { useEdicao } from "@/hooks/useEdicao";
+import { useEffect, useState } from "react";
+import { edicaoApi } from "@/services/edicao";
 
 export default function Edicoes() {
-  const { title, userArea, cardsMock } = EventoMock;
   const { deleteEdicao, loadingEdicoesList } = useEdicao();
   const router = useRouter();
+  const [edicoes, setEdicoes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEdicoes = async () => {
+      try {
+        const data = await edicaoApi.listEdicao();
+        setEdicoes(data);
+      } catch (error) {
+        console.error("Erro ao carregar as edições:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEdicoes();
+  }, []);
 
   return (
     <div
@@ -20,10 +37,10 @@ export default function Edicoes() {
         <p>Carregando edições...</p>
       ) : (
         <Listagem
-          title={title}
-          labelAddButton={userArea.add}
-          searchPlaceholder={userArea.search}
-          cardsList={cardsMock}
+          title={"Edições do Evento"}
+          labelAddButton={"Cadastrar Edição"}
+          searchPlaceholder={"Pesquise por edição"}
+          cardsList={edicoes}
           onDelete={(id: string) => deleteEdicao(id)}
           onAddButtonClick={() => router.push("/cadastro-edicao")}
         />
