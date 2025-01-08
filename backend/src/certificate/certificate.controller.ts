1dea8be5a7882e0abc17b2e1d7cc0eacc3c00b76
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2025-01-08 20:02:43 -0300

    feat: V0 of route that returns PDF

--- backend/src/certificate/certificate.controller.ts
+++ backend/src/certificate/certificate.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Header, Res } from '@nestjs/common';
 import { CertificateService } from './certificate.service';
+import { Response } from 'express';
 
 @Controller('certificate')
 export class CertificateController {
   constructor(private readonly certificateService: CertificateService) {}
+
+  @Get()
+  @Header('Content-Type', 'application/pdf')
+  async helloWorld(@Res() res: Response) {
+    const pdfBuffer = await this.certificateService.helloWorld();
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename=certificate.pdf',
+    });
+    res.send(pdfBuffer);
+  }
 }
--- backend/src/certificate/certificate.service.ts
+++ backend/src/certificate/certificate.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import PDFDocument from 'pdfkit';
 
 @Injectable()
-export class CertificateService {}
+export class CertificateService {
+  constructor(private prismaClient: PrismaService) {}
+
+  async helloWorld(): Promise<Buffer> {
+    return new Promise((resolve) => {
+      const doc = new PDFDocument({
+        layout: 'landscape',
+        size: 'A4',
+      });
+      const chunks: Buffer[] = [];
+
+      doc.on('data', (chunk) => chunks.push(chunk));
+      doc.on('end', () => resolve(Buffer.concat(chunks)));
+
+      doc.text('Hello World');
+      doc.end();
+    });
+  }
+}
