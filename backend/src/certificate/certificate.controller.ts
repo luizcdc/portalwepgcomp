32eea1ff2f7911b3d2768b2a3df9fce304cb6f31
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2025-01-11 11:55:37 -0300

    feat: Implement queries for identifying what to include in user's certificate

--- backend/src/certificate/certificate.controller.ts
+++ backend/src/certificate/certificate.controller.ts
@@ -1,15 +1,35 @@
-import { Controller, Get, Header, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Param,
+  Request,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { CertificateService } from './certificate.service';
 import { Response } from 'express';
-
+import { UserLevels } from '../auth/decorators/user-level.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UserLevelGuard } from '../auth/guards/user-level.guard';
+import { UserLevel } from '@prisma/client';
 @Controller('certificate')
+@UseGuards(JwtAuthGuard, UserLevelGuard)
 export class CertificateController {
   constructor(private readonly certificateService: CertificateService) {}
 
-  @Get()
+  @Get('event-edition/:eventEditionId')
   @Header('Content-Type', 'application/pdf')
-  async helloWorld(@Res() res: Response) {
-    const pdfBuffer = await this.certificateService.helloWorld();
+  @UserLevels(UserLevel.Superadmin, UserLevel.Admin, UserLevel.Default)
+  async downloadCertificate(
+    @Request() req: any,
+    @Res() res: Response,
+    @Param('eventEditionId') eventEditionId: string,
+  ) {
+    const pdfBuffer = await this.certificateService.generateCertificateForUser(
+      req.user.userId,
+      eventEditionId,
+    );
     res.set({
       'Content-Type': 'application/pdf',
       'Content-Disposition': 'attachment; filename=certificate.pdf',
--- backend/src/certificate/certificate.service.ts
+++ backend/src/certificate/certificate.service.ts
@@ -1,12 +1,82 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import PDFDocument from 'pdfkit';
+import { AppException } from '../exceptions/app.exception';
+import { Profile } from '@prisma/client';
 
 @Injectable()
 export class CertificateService {
   constructor(private prismaClient: PrismaService) {}
 
-  async helloWorld(): Promise<Buffer> {
+  async generateCertificateForUser(
+    userId: string,
+    eventEditionId: string,
+  ): Promise<Buffer> {
+    // Info to get: nome, current event's presentation title, current event's title, current event's start and end date.
+    // Whether they were awarded or not. (just get the 3 max score submissions and see if theirs is one of them)
+    // TODO: Use Certificate database row to return existing file, if it exists.
+    const user = await this.prismaClient.userAccount.findUnique({
+      where: { id: userId },
+      include: {
+        panelistParticipations: {
+          where: {
+            presentationBlock: {
+              eventEditionId: eventEditionId,
+            },
+          },
+        },
+        mainAuthored: {
+          where: {
+            eventEditionId,
+          },
+        },
+        panelistAwards: {
+          where: {
+            eventEditionId,
+          },
+        },
+      },
+    });
+    const eventEdition = await this.prismaClient.eventEdition.findUnique({
+      where: { id: eventEditionId },
+      include: {
+        certificates: true,
+        // TODO: when prisma is fixed, use this instead of the above line
+        // certificates: {
+        //   where: { userId },
+        // }
+      },
+    });
+    console.log(user);
+    console.log(eventEdition);
+    if (!user) {
+      throw new AppException('Usuário não encontrado', 404);
+    } else if (!eventEdition) {
+      throw new AppException('Edição do evento não encontrada', 404);
+    }
+    if (user.profile === Profile.Listener) {
+      throw new AppException(
+        'Usuário não participou como apresentador ou avaliador, portanto não pode receber certificado',
+        400,
+      );
+    } else if (
+      user.profile === Profile.DoctoralStudent &&
+      !user.mainAuthored?.length
+      // TODO: check presentation status
+    ) {
+      throw new AppException(
+        'Doutorando não tem submissões, portanto não pode receber certificado',
+        400,
+      );
+    } else if (
+      user.profile === Profile.Professor &&
+      !user.panelistParticipations?.length
+    ) {
+      throw new AppException(
+        'Professor não foi premiado, portanto não pode receber certificado',
+        400,
+      );
+    }
     return new Promise((resolve) => {
       const doc = new PDFDocument({
         layout: 'landscape',
