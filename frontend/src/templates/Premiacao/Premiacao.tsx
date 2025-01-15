8f5a3c292db6842b97f0f5f3e0deb45d7b3eb521
Lucas Azevedo <lucasazevedooliveira@outlook.com.br>
2025-01-15 16:07:25 -0300

    styles

--- frontend/src/templates/Premiacao/Premiacao.tsx
+++ frontend/src/templates/Premiacao/Premiacao.tsx
@@ -32,7 +32,7 @@ export default function Premiacao({
 
   return (
     <div className='d-grid gap-3 mb-5 premiacao'>
-      <div className='ms-5'>
+      <div className='d-flex flex-column'>
         <h1 className='fw-bold title'>
           {categoria === "banca"
             ? " Melhores Apresentações - Banca"
@@ -53,70 +53,71 @@ export default function Premiacao({
                     }
                     `}
         </h5>
-        {premiacoes.length === 0 && categoria !== "avaliadores" ? (
-          <div className='d-flex align-items-center justify-content-center p-3 mt-4 me-5'>
-            <h4 className='empty-list mb-0'>
-              <Image
-                src='/assets/images/empty_box.svg'
-                alt='Lista vazia'
-                width={90}
-                height={90}
-              />
-              Essa lista ainda está vazia
-            </h4>
-          </div>
-        ) : premiacoes.length === 0 && categoria === "avaliadores" && premiacoesValuesAvaliadores !== undefined ?  (
-          premiacoesValuesAvaliadores.map((item, index) => (
-            <div
-              key={index}
-              className='d-flex align-items-center justify-content-between border border-3 border-solid custom-border p-3 mt-4 me-5'
-            >
-              <div className='text-black'>
-                <h6
-                  className={`text-black fw-semibold ${
-                    !item.name ? "mb-0" : ""
-                  }`}
-                >
-                  {item.name}
-                </h6>
-                {item.email && (
-                  <h6 className='text-black mb-0'>{item.email}</h6>
-                )}
-              </div>
+        <div className='d-flex flex-column gap-3'>
+          {premiacoes.length === 0 && categoria !== "avaliadores" ? (
+            <div className='d-flex align-items-center justify-content-center p-3'>
+              <h4 className='empty-list mb-0'>
+                <Image
+                  src='/assets/images/empty_box.svg'
+                  alt='Lista vazia'
+                  width={90}
+                  height={90}
+                />
+                Essa lista ainda está vazia
+              </h4>
             </div>
-          ))
-        ) : premiacoesValues !== undefined ?  (
-          premiacoesValues.map((item, index) => (
-            <div
-              key={index}
-              className='d-flex align-items-center justify-content-between border border-3 border-solid custom-border p-3 mt-4 me-5'
-            >
-              <div className='text-black'>
-                <h6
-                  className={`text-black fw-semibold ${
-                    !item.submission.mainAuthor.name ? "mb-0" : ""
-                  }`}
-                >
-                  {item.submission.title}
-                </h6>
-                {item.submission.mainAuthor.name && (
-                  <h6 className='text-black mb-0'>
-                    {item.submission.mainAuthor.name}
+          ) : premiacoes.length === 0 && categoria === "avaliadores" && premiacoesValuesAvaliadores !== undefined ?  (
+            premiacoesValuesAvaliadores.map((item, index) => (
+              <div
+                key={index}
+                className='d-flex align-items-center justify-content-between border border-3 border-solid custom-border p-3'
+              >
+                <div className='text-black'>
+                  <h6
+                    className={`text-black fw-semibold ${
+                      !item.name ? "mb-0" : ""
+                    }`}
+                  >
+                    {item.name}
                   </h6>
-                )}
+                  {item.email && (
+                    <h6 className='text-black mb-0'>{item.email}</h6>
+                  )}
+                </div>
               </div>
-              <div className='text-end'>
-                <h4 className='mb-0 text-black fw-bold'>
-                  {categoria === "banca"
-                    ? item.evaluatorsAverageScore
-                    : categoria === "publico"
-                    ? item.publicAverageScore
-                    : ""}
-                </h4>
+            ))
+          ) : premiacoesValues !== undefined ?  (
+            premiacoesValues.map((item, index) => (
+              <div
+                key={index}
+                className='d-flex align-items-center justify-content-between border border-3 border-solid custom-border p-3'
+              >
+                <div className='text-black'>
+                  <h6
+                    className={`text-black fw-semibold ${
+                      !item.submission.mainAuthor.name ? "mb-0" : ""
+                    }`}
+                  >
+                    {item.submission.title}
+                  </h6>
+                  {item.submission.mainAuthor.name && (
+                    <h6 className='text-black mb-0'>
+                      {item.submission.mainAuthor.name}
+                    </h6>
+                  )}
+                </div>
+                <div className='text-end'>
+                  <h4 className='mb-0 text-black fw-bold'>
+                    {categoria === "banca"
+                      ? item.evaluatorsAverageScore
+                      : categoria === "publico"
+                      ? item.publicAverageScore
+                      : ""}
+                  </h4>
+                </div>
               </div>
-            </div>
-          ))
-        ) :
+            ))
+          ) :
         <div className='d-flex align-items-center justify-content-center p-3 mt-4 me-5'>
           <h4 className='empty-list mb-0'>
             <Image
@@ -129,6 +130,7 @@ export default function Premiacao({
           </h4>
         </div>
         }
+        </div>
       </div>
     </div>
   );
