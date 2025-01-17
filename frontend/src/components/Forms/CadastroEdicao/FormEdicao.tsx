7c953fe361731e518fe027de3ec07840d0842d6a
Felipe Rezende <40390482+Rezende123@users.noreply.github.com>
2025-01-16 21:38:01 -0300

    Corrige compatibilidade de datas ao Editar Edição do Evento (#352)
    Fix #344

--- frontend/src/components/Forms/CadastroEdicao/FormEdicao.tsx
+++ frontend/src/components/Forms/CadastroEdicao/FormEdicao.tsx
@@ -155,8 +155,8 @@ export function FormEdicao({ edicaoData }: Readonly<FormEdicao>) {
     if (edicaoData && Object.keys(edicaoData).length) {
       setValue("titulo", edicaoData.name);
       setValue("descricao", edicaoData.description);
-      setValue("inicio", dayjs(edicaoData.startDate).toDate().toString());
-      setValue("final", dayjs(edicaoData.endDate).toDate().toString());
+      setValue("inicio", edicaoData.startDate); //dayjs(edicaoData.startDate).toDate().toString());
+      setValue("final", edicaoData.endDate); //dayjs(edicaoData.endDate).toDate().toString());
       setValue("local", edicaoData.location);
       setValue("coordinatorId", user?.id);
       setValue(
