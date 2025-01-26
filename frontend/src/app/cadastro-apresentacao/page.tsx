91d3539bce4fa812d52c6682c83c79408eac32f3
GabrielCdev <gabi_borges01@hotmail.com>
2025-01-26 12:35:56 -0300

    feat: Added rule to redirect to /home in 'cadastro-apresentacao' page

--- frontend/src/app/cadastro-apresentacao/page.tsx
+++ frontend/src/app/cadastro-apresentacao/page.tsx
@@ -1,17 +1,40 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
+
+import { useSweetAlert } from "@/hooks/useAlert";
+import { useUsers } from "@/hooks/useUsers";
+
+import { AuthContext } from "@/context/AuthProvider/authProvider";
+import { SubmissionProvider } from "@/context/submission";
+import { SubmissionFileProvider } from "@/context/submissionFile";
+
 import { FormCadastroApresentacao } from "@/components/Forms/CadastroApresentacao/FormCadastroApresentacao";
 import LoadingPage from "@/components/LoadingPage";
 import ModalCadastroApresentacao from "@/components/Modals/ModalCadastroApresentacao/ModalCadastroApresentacao";
-import { SubmissionProvider } from "@/context/submission";
-import { SubmissionFileProvider } from "@/context/submissionFile";
-import { useUsers } from "@/hooks/useUsers";
+import { ProtectedLayout } from "@/components/ProtectedLayout/protectedLayout";
 
 import "./style.scss";
-import { ProtectedLayout } from "@/components/ProtectedLayout/protectedLayout";
 
 export default function CadastroApresentacao() {
   const { loadingCreateUser } = useUsers();
+  const { user } = useContext(AuthContext);
+  const { showAlert } = useSweetAlert();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user?.profile !== "DoctoralStudent") {
+      showAlert({
+        icon: "error",
+        title: "Acesso não autorizado",
+        text: "Você não tem permissão para acessar esta página.",
+        confirmButtonText: "OK",
+      }).then(() => {
+        router.push("/");
+      });
+    }
+  }, [])
 
   return (
     <ProtectedLayout>
@@ -19,7 +42,7 @@ export default function CadastroApresentacao() {
         <SubmissionProvider>
           <div className="container d-flex flex-column flex-grow-1 text-black pageApresentacao">
             {loadingCreateUser && <LoadingPage />}
-            {!loadingCreateUser && (
+            {!loadingCreateUser && user?.profile === "DoctoralStudent" && (
               <>
                 <div className="container">
                   <h1 className="d-flex justify-content-center mt-5 fw-normal">
