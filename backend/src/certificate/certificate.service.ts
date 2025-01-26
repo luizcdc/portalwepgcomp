8f140ae3825136754e14f276ab12dce7b8fa3180
ernestosrf <ernestosrf98@gmail.com>
2025-01-26 17:38:06 -0300

    fix: update font paths in CertificateService for correct referencing

--- backend/src/certificate/certificate.service.ts
+++ backend/src/certificate/certificate.service.ts
@@ -12,12 +12,13 @@ import { MailingService } from '../mailing/mailing.service';
 @Injectable()
 export class CertificateService {
   private readonly fontsPaths = {
-    bold: '../assets/SourceSerif4-Bold.ttf',
-    regular: '../assets/SourceSerif4-Regular.ttf',
-    semibold: '../assets/SourceSerif4-SemiBold.ttf',
-    italic: '../assets/SourceSerif4-Italic.ttf',
-    medium: '../assets/SourceSerif4-Medium.ttf',
-    bolditalic: '../assets/SourceSerif4-BoldItalic.ttf',
+    bold: './assets/SourceSerif4-Bold.ttf',
+    regular: './assets/SourceSerif4-Regular.ttf',
+    semibold: './assets/SourceSerif4-SemiBold.ttf',
+    italic: './assets/SourceSerif4-Italic.ttf',
+    medium: './assets/SourceSerif4-Medium.ttf',
+    // bolditalic: 'src/certificate/assets/SourceSerif4-BoldItalic.ttf',
+    bolditalic: './assets/SourceSerif4-BoldItalic.ttf',
   };
   constructor(
     private prismaClient: PrismaService,
