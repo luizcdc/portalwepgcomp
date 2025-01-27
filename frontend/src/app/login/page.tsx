629d654e55d4fe351115c5680420b2e2b675ce07
jubsribs <julianacrispina@gmail.com>
2025-01-27 11:31:37 -0300

    fix: ajuste titulo para dinamico

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,38 +3,39 @@
 import { FormLogin } from "@/components/Forms/Login/FormLogin";
 import ModalAlterarSenha from "@/components/Modals/ModalAlterarSenha/ModalAlterarSenha";
 import "./style.scss";
+import { useEdicao } from "@/hooks/useEdicao";
 
 export default function Login() {
+  const { Edicao } = useEdicao();
   return (
-    <div className="container d-flex flex-column flex-grow-1 text-black">
-      <div className="container">
-        <h1 className="d-flex justify-content-center mt-5 fw-normal border-yellow">
-          WEPGCOMP
-          <span className="ms-2">2025</span>
+    <div className='container d-flex flex-column flex-grow-1 text-black'>
+      <div className='container'>
+        <h1 className='d-flex justify-content-center mt-5 fw-normal border-yellow ms-2'>
+          {Edicao?.name || "Carregando..."}
         </h1>
         <hr />
-        <h4 className="d-flex justify-content-center mb-4 fw-semibold fs-4">
+        <h4 className='d-flex justify-content-center mb-4 fw-semibold fs-4'>
           Acesse sua conta
         </h4>
       </div>
 
-      <div className="container d-flex flex-column">
-        <div className="row">
-          <div className="col-12">
-            <div className="container d-flex justify-content-center">
+      <div className='container d-flex flex-column'>
+        <div className='row'>
+          <div className='col-12'>
+            <div className='container d-flex justify-content-center'>
               <FormLogin />
             </div>
           </div>
         </div>
       </div>
 
-      <div className="container d-flex justify-content-center mb-4  flex-grow-1">
-        <div className="text-start fixed-width">
+      <div className='container d-flex justify-content-center mb-4  flex-grow-1'>
+        <div className='text-start fixed-width'>
           <h6>
             Ainda não tem conta?
             <a
-              href="/cadastro"
-              className="link-underline link-underline-opacity-0 ms-1"
+              href='/cadastro'
+              className='link-underline link-underline-opacity-0 ms-1'
             >
               Cadastre-se
             </a>
